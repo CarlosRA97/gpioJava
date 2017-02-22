596b816da757459f3b2c9bbba57c006f09822dac
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2017-02-22 13:01:29 +0100

    init commit

---
+++ src/main/java/com/zonkware/pi4led/Application.java
@@ -0,0 +1,19 @@
+package com.zonkware.pi4led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Carlos on 22/2/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        SpringApplication.run(Application.class, args);
+
+    }
+}
